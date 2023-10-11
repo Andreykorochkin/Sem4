eeabd4398b11e124e1904b83fc63d5db8033b7c7
Andreykorochkin <136254104+Andreykorochkin@users.noreply.github.com>
2023-10-11 17:26:03 +0300

    add homework

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную 
+// степень B.
+
+Console.WriteLine($"ВВедите число А: и B: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int Power(int a, int b )
+{
+    int Power = 1;
+    for (int i = 0; i <= b; i++)
+    {
+         Power *= b;
+    }
+    return Power;
+}
+
+Console.WriteLine($"Число {a} в степени {b} равна {Power(a,b)}");
